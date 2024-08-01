18214d9d872ca17b32c2f28defe203edcddbf6d1
sahil99811 <sp220253@gmail.com>
2024-08-01 08:27:53 +0530

    boiler plate code

--- server/server.js
+++
@@ -1,7 +0,0 @@
-const express=require('express');
-const app=express();
-const dotenv=require('dotenv');
-dotenv.config();
-app.listen(process.env.PORT,()=>{
-    console.log("server is started");
-})
